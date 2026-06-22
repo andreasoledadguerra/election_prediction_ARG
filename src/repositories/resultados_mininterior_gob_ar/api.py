cfd9d1a40879b653acafecb82e724bcadf98487e
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-06-22 11:11:09 -0300

    add parametric function to set API' s endpoint

---
+++ src/repositories/resultados_mininterior_gob_ar/api.py
@@ -0,0 +1,41 @@
+import os
+import requests
+
+BASE_URL = "https://resultados.mininterior.gob.ar/api/resultados/getResultados"
+
+
+def get_resultados(
+    categoria_id: int,              # 1=Presidente, 2=Diputado Nacional, 3=Intendente
+    anio_eleccion: str = None,
+    tipo_eleccion: str = None,      # "1"=PASO, "2"=Generales, "3"=Segunda Vuelta
+    tipo_recuento: str = None,      #"1" (único)
+    distrito_id: str = None,        # datos geográficos: si no se especifican la API devuelve resultadps nacionales agregados
+    seccion_provincial_id: str = None,
+    seccion_id: str = None,
+    circuito_id: str = None,
+    mesa_id: str = None,
+) -> dict:
+
+    token = os.getenv("MININTERIOR_TOKEN")
+    if not token:
+        raise EnvironmentError("Falta MININTERIOR_TOKEN en las variables de entorno.")
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    params = {
+        "categoriaId": categoria_id,
+        "anioEleccion": anio_eleccion,
+        "tipoEleccion": tipo_eleccion,
+        "tipoRecuento": tipo_recuento,
+        "distritoId": distrito_id,
+        "seccionProvincialId": seccion_provincial_id,
+        "seccionId": seccion_id,
+        "circuitoId": circuito_id,
+        "mesaId": mesa_id,
+    }
+    # saca los None para no mandar parámetros vacíos
+    params = {k: v for k, v in params.items() if v is not None}
+
+    response = requests.get(BASE_URL, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json() #dict con la respuesta JSON de la API
